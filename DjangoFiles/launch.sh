2bc3dd0f16af7369946d34c94f69e3fed8f41596
Jonas 12t <jturbeaux@pm.me>
2022-02-08 14:22:18 +0400

    touch /DjangoFiles/www/Djangologfile

--- DjangoFiles/launch.sh
+++ DjangoFiles/launch.sh
@@ -1,2 +1,8 @@
+mkdir -p /DjangoFiles/www
+touch /DjangoFiles/www/nginxAccess.log
+touch /DjangoFiles/www/nginxError.log
+touch /DjangoFiles/www/gunicorn.logs
 touch /DjangoFiles/www/Djangologfile
+
+
 sleep 30d
