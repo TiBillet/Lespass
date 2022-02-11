228b26b3719dc9a5d5ed441232b6d21f3ba14c24
Jonas 12t <jturbeaux@pm.me>
2022-02-11 15:49:51 +0400

    Me Serializer

--- DjangoFiles/AuthBillet/serializers.py
+++ DjangoFiles/AuthBillet/serializers.py
@@ -1,22 +1,6 @@
-import datetime
-
-from django.contrib.auth import get_user_model
-from django.db import connection
-from django.utils.text import slugify
 from rest_framework import serializers
-import json
-from django.utils.translation import gettext, gettext_lazy as _
-from rest_framework.generics import get_object_or_404
-from django_tenants.utils import schema_context, tenant_context
-
-from AuthBillet.models import TibilletUser, HumanUser
-from BaseBillet.models import Event, Price, Product, Reservation, Configuration, LigneArticle, Ticket, Paiement_stripe, \
-    PriceSold, ProductSold, Artist_on_event
-from Customers.models import Client
-from PaiementStripe.views import creation_paiement_stripe
-
+from AuthBillet.models import TibilletUser
 import logging
-
 logger = logging.getLogger(__name__)
 
 
