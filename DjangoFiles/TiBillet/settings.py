e447a292bdaffe53d59e629bffab8d1f93c91823
Jonas 12t <jturbeaux@pm.me>
2022-02-14 09:35:32 +0400

    token valide 1 jour

--- DjangoFiles/TiBillet/settings.py
+++ DjangoFiles/TiBillet/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
