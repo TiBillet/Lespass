bb1735beca92dc41c99800017511db953644c04f
slaan <jturbeaux@pm.me>
2022-03-10 13:01:27 +0100

    product adhésion if boutton adhésion coché

--- DjangoFiles/ApiBillet/views.py
+++ DjangoFiles/ApiBillet/views.py
@@ -292,7 +292,7 @@ class HereViewSet(viewsets.ViewSet):
         dict_return = {'uuid': f"{connection.tenant.uuid}"}
         dict_return.update(place_serialized.data)
 
-        if config.button_adhesion :
+        if config.button_adhesion or config.adhesion_obligatoire :
             products_adhesion = Product.objects.filter(categorie_article=Product.ADHESION)
             if len( products_adhesion ) > 0 :
                 products_serializer = ProductSerializer(products_adhesion, many=True)
