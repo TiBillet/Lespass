263d89cea519eb31943544e0fa62060a31433eab
Jonas Legion <jturbeaux@pm.me>
2022-05-18 08:26:08 +0400

    Status place traduction

--- Front/src/views/StatusPlace.vue
+++ Front/src/views/StatusPlace.vue
@@ -239,7 +239,7 @@
 console.log('-> StatusPlace.vue !')
 
 // traduction
-import {trad} from '@/divers/translation'
+// import {trad} from '@/divers/translation'
 
 function goTo(anchor) {
   console.log('-> fonc goTo, anchor =', anchor)
