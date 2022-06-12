9bee5f00495541f6ddfd9e9b14780eda84db99bf
Jonas Legion <jturbeaux@pm.me>
2022-06-12 09:33:19 +0400

    prod et sso test

--- DjangoFiles/AuthBillet/views.py
+++ DjangoFiles/AuthBillet/views.py
@@ -216,8 +216,8 @@ class OAauthApi(APIView):
             settings.OAUTH_CLIENT_NAME, overwrite=True, **settings.OAUTH_CLIENT, update_token=update_token
         )
 
-        redirect_base_url = f"https://{connection.tenant.get_primary_domain().domain}"
-        logger.info(f"redirect_base_url : {redirect_base_url}/api/user/oauth")
+        redirect_base_url = f"https://{connection.tenant.get_primary_domain().domain}/api/user/oauth"
+        logger.info(f"redirect_base_url : {redirect_base_url}")
 
         auth = sso_client.authorize_redirect(request, redirect_base_url)
         # auth = sso_client.authorize_redirect(request, settings.OAUTH_CLIENT['redirect_uri'])
