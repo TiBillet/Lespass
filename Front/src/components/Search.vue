ee7e4a35e60456da73a25bd95001db34cbd8e116
icon <filaos974@hotmail.com>
2022-08-11 10:58:06 +0400

    creéation du composant "Search"

---
+++ Front/src/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row bg-white shadow mt-n5 border-radius-lg pb-4 p-3 position-relative w-75 mx-auto">
+        <div class="col-lg-8 mt-lg-n2 mt-2">
+            <label></label>
+            <div class="input-group">
+                <span class="input-group-text">
+                    <i class="fas fa-search" aria-hidden="true"></i>
+                </span>
+                <input v-model.lazy="research" class="form-control" placeholder="Rechechez un évènement" type="text" />
+            </div>
+        </div>
+        <div class="col-lg-4 d-flex align-items-center mt-lg-auto mt-2">
+            <button type="button" class="btn bg-gradient-primary w-100 mb-0">Rechercher</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// store
+import { storeToRefs } from 'pinia'
+import { useAllStore } from '@/stores/all'
+
+const { events, place } = storeToRefs(useAllStore())
+
+let research = ''
+</script>
+
+<style>
+</style>
