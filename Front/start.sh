ce325ff97cd332d29abb0bcf9c7f65dc55b6d7eb
Jonas Legion <jturbeaux@pm.me>
2022-10-14 09:47:15 +0400

    cp assets

--- Front/start.sh
+++ Front/start.sh
@@ -1,10 +1,10 @@
 # Petit soucis de licence qui empêche
 # certains assets d'être up sur gituhb.
 # On s'en occupe bientôt, promis !
-cp -r /nonfree/assets/css/now-design* /Front/assets/css/
-cp -r /nonfree/assets/css/nucleo* /Front/assets/css/
-cp -r /nonfree/assets/js/now-design* /Front/assets/js/
-cp -r /nonfree/assets/fonts/nucleo* /Front/assets/fonts/
+cp -r /nonfree/css/now-design* /Front/assets/css/
+cp -r /nonfree/css/nucleo* /Front/assets/css/
+cp -r /nonfree/js/now-design* /Front/assets/js/
+cp -r /nonfree/fonts/nucleo* /Front/assets/fonts/
 
 npm install
 #npm run dev
