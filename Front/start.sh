a0861580e5d36eb0a15ceeae31fa77eb6268df12
Jonas Legion <jturbeaux@pm.me>
2022-10-14 09:21:52 +0400

    add dockerfile for nonfree assets

--- Front/start.sh
+++ Front/start.sh
@@ -1,3 +1,11 @@
-#npm install
+# Petit soucis de licence qui empêche
+# certains assets d'être up sur gituhb.
+# On s'en occupe bientôt, promis !
+cp -r /nonfree/assets/css/now-design* /Front/assets/css/
+cp -r /nonfree/assets/css/nucleo* /Front/assets/css/
+cp -r /nonfree/assets/js/now-design* /Front/assets/js/
+cp -r /nonfree/assets/fonts/nucleo* /Front/assets/fonts/
+
+npm install
 #npm run dev
 sleep 30d
