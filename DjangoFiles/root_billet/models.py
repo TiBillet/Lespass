bb631615c5036cc184fc82e227024752ea373fbf
Jonas Legion <jturbeaux@pm.me>
2022-11-21 13:16:21 +0400

    add root app for shared config

--- DjangoFiles/root_billet/models.py
+++ DjangoFiles/root_billet/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+from solo.models import SingletonModel
+
+
+class RootConfiguration(SingletonModel):
+
+
+    TZ_REUNION, TZ_PARIS = "Indian/Reunion", "Europe/Paris"
+    TZ_CHOICES = [
+        (TZ_REUNION, _('Indian/Reunion')),
+        (TZ_PARIS, _('Europe/Paris')),
+    ]
+
+    fuseau_horaire = models.CharField(default=TZ_REUNION,
+                                      max_length=50,
+                                      choices=TZ_CHOICES,
+                                      )
+
+    stripe_api_key = models.CharField(max_length=110, blank=True, null=True)
+    stripe_test_api_key = models.CharField(max_length=110, blank=True, null=True)
+
+    stripe_mode_test = models.BooleanField(default=True)
+
+    def get_stripe_api(self):
+        if self.stripe_mode_test:
+            return self.stripe_test_api_key
+        else:
+            return self.stripe_api_key
