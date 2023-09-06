9201b64e427e38ca563a8620cd447c496658266f
Jonas Legion <jturbeaux@pm.me>
2023-09-06 10:31:58 +0200

    Abonnement = O

--- DjangoFiles/BaseBillet/models.py
+++ DjangoFiles/BaseBillet/models.py
@@ -468,7 +468,7 @@ class Product(models.Model):
 
     NONE, BILLET, PACK, RECHARGE_CASHLESS = 'N', 'B', 'P', 'R'
     RECHARGE_FEDERATED, VETEMENT, MERCH, ADHESION = 'S', 'T', 'M', 'A'
-    ABONNEMENT, DON, FREERES = 'B', 'D', 'F'
+    ABONNEMENT, DON, FREERES = 'O', 'D', 'F'
 
     CATEGORIE_ARTICLE_CHOICES = [
         (NONE, _('Selectionnez une catégorie')),
