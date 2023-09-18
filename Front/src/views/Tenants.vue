441309a50ff4d62e1ba8c853b21c4e6fd92c31f5
filaos974 <filaos974@hotmail.com>
2023-09-18 12:57:52 +0400

    modi html, css et remplacement du css wizard

--- Front/src/views/Tenants.vue
+++ Front/src/views/Tenants.vue
@@ -1,55 +1,56 @@
 <template>
-  <div class="container-fluid vw-100 vh-100" :style="`background-image: url('${wizardBackground}');background-repeat:no-repeat;background-size:cover;`">
+  <div class="container-fluid vw-100 vh-100 d-flex justify-content-center align-items-center"
+       :style="`background-image: url('${wizardBackground}');background-repeat:no-repeat;background-size:100% 100%;`">
     <div class="container">
-      <!--      Wizard container        -->
-      <div class="wizard-container">
-        <div class="card wizard-card" data-color="red" id="wizard">
-          <form action="" method="">
-            <div class="wizard-header">
-              <h3 class="wizard-title">
-                Créer votre espace
-              </h3>
-              <h5>Sélectionner, éditer un type d'espace.</h5>
-            </div>
-            <!-- navigation -->
-            <div class="wizard-navigation">
-              <ul class="nav nav-pills">
-                <li :style="{width: itemNavWidth + '%'}">
-                  <a href="#espace" class="nav-item-tab" style="font-size:12px !important;font-weight:500 !important;color: #555555 !important;padding:10px 15px !important;">
-                    Espace
-                  </a>
-                </li>
-                <li :style="{width: itemNavWidth + '%'}"><a href="#informations" class="h3">Informations</a></li>
-                <li :style="{width: itemNavWidth + '%'}"><a href="#validation" class="nav-item-tab">Validation</a></li>
-              </ul>
-              <div class="moving-tab" :style="styleBtMobile">
+      <div class="card wizard-card" data-color="red">
+        <form action="" method="">
+          <div class="wizard-header">
+            <h3 class="wizard-title">
+              Créer votre espace
+            </h3>
+            <h5>Sélectionner, éditer un type d'espace.</h5>
+          </div>
+          <!-- navigation -->
+          <div class="wizard-navigation">
+            <ul class="nav nav-pills">
+              <li :style="{width: itemNavWidth + '%'}" data-cible="espace" class="tab-nav-item">
                 Espace
-              </div>
+              </li>
+              <li :style="{width: itemNavWidth + '%'}" data-cible="informations" class="tab-nav-item">
+                Informations
+              </li>
+              <li :style="{width: itemNavWidth + '%'}" data-cible="validation" class="tab-nav-item">
+                Validation
+              </li>
+            </ul>
+            <div class="bt-tab btn-wizard" :style="styleBtMobile">
+              Espace
             </div>
-            <div class="tab-content ps-3 pe-3">
-              <div id="espace" class="tab-pane active">
-                contenu espace
-              </div>
-              <div id="information" class="tab-pane">
-                contenu information
-              </div>
-              <div id="validation" class="tab-pane">
-                contenu validation
-              </div>
+          </div>
+          <div class="tab-content ps-3 pe-3">
+            <div id="espace" class="tab-pane active">
+              contenu espace
             </div>
+            <div id="information" class="tab-pane">
+              contenu information
+            </div>
+            <div id="validation" class="tab-pane">
+              contenu validation
+            </div>
+          </div>
 
+          <div class="d-flex flex-row justify-content-between wizard-footer">
+            <div class="pull-left pe-3">
+              <input type="button" class="btn btn-wizard btn-previous" name="previous" value="Previous">
+            </div>
             <div class="pull-right ps-3">
-              <input type="button" class="btn btn-next btn-fill btn-danger btn-wd" name="next" value="Next" style="">
+              <input type="button" class="btn btn-wizard btn-next btn-fill btn-danger btn-wd" name="next" value="Next"
+                     style="">
               <input type="button" class="btn btn-finish btn-fill btn-danger btn-wd" name="finish" value="Finish"
                      style="display: none;">
             </div>
-            <div class="pull-left pe-3">
-              <input type="button" class="btn btn-previous btn-fill btn-default btn-wd disabled" name="previous"
-                     value="Previous">
-            </div>
-
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -57,12 +58,11 @@
 
 <script setup>
 console.log('-> Tenants.vue')
-import { onMounted, ref, reactive } from 'vue'
+import { onMounted, onBeforeUnmount, ref, reactive } from 'vue'
 import { useSessionStore } from '@/stores/session'
 
 // material-bootstrap-wizard
 import wizardBackground from '../assets/img/wizard-profile.jpg'
-import '../assets/css/material-bootstrap-wizard/material-bootstrap-wizard.css'
 
 // session store
 const sessionStore = useSessionStore()
@@ -80,7 +80,7 @@ updateHeader(null)
 function moveBt (event) {
   const ele = event.target
   // text du bouton mobile
-  document.querySelector('div[class="moving-tab"]').innerText = ele.innerText
+  document.querySelector('div[class~="bt-tab"]').innerText = ele.innerText
   // animation
   const index = parseInt(ele.getAttribute('index'))
   const nbItem = document.querySelectorAll('ul[class="nav nav-pills"] li').length
@@ -90,7 +90,7 @@ function moveBt (event) {
   if (index === 0) {
     decX = -8
   }
-  if (index+1 === nbItem) {
+  if (index + 1 === nbItem) {
     decX = 8
   }
   styleBtMobile.value.transform = `translate3d(${decX + (index * navWidth)}px, 0px, 0px)`
@@ -101,24 +101,100 @@ function moveBt (event) {
   ele.classList.add('active')
 }
 
-function init() {
+function init () {
   const itemNavs = document.querySelectorAll('ul[class="nav nav-pills"] li')
   itemNavWidth.value = (100 / itemNavs.length).toFixed(3)
   styleBtMobile.value.width = itemNavWidth.value + '%'
 
-  console.log('itemNavWidth.value =', itemNavWidth.value)
-  console.log('styleBtMobile.value =', styleBtMobile)
+  // console.log('itemNavWidth.value =', itemNavWidth.value)
+  // console.log('styleBtMobile.value =', styleBtMobile)
   for (let i = 0; i < itemNavs.length; i++) {
     const item = itemNavs[i]
-    item.querySelector('a').setAttribute('index', i)
+    item.setAttribute('index', i)
     item.addEventListener('click', moveBt)
   }
 }
 
-addEventListener("resize", init);
+addEventListener('resize', init)
 
 onMounted(() => init())
+
+onBeforeUnmount(() => {
+  const itemNavs = document.querySelectorAll('ul[class="nav nav-pills"] li')
+  for (let i = 0; i < itemNavs.length; i++) {
+    const item = itemNavs[i]
+    item.removeEventListener('click', moveBt)
+  }
+  removeEventListener('resize', init)
+})
 </script>
 
 <style scoped>
+.wizard-card {
+  min-height: 410px;
+  box-shadow: 0 16px 24px 2px rgba(0, 0, 0, .14), 0 6px 30px 5px rgba(0, 0, 0, .12), 0 8px 10px -5px rgba(0, 0, 0, .2);
+}
+
+.wizard-card, .wizard-header {
+  text-align: center;
+  padding: 25px 0 35px;
+}
+
+.wizard-title {
+  font-weight: 700;
+}
+
+.wizard-navigation {
+  position: relative;
+}
+
+.tab-content {
+  min-height: 340px;
+  padding: 20px 15px;
+}
+
+.bt-tab {
+  position: absolute;
+  left: 0;
+  top: -2px;
+  text-align: center;
+  padding: 14px 12px;
+  background-color: #f44336;
+}
+
+.tab-nav-item {
+  border: 0 !important;
+  border-radius: 0;
+  line-height: 18px;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 500;
+  min-width: 100px;
+  text-align: center;
+  color: #555 !important;
+  padding: 12px !important;
+  cursor: pointer;
+}
+
+.btn-wizard {
+  font-size: 12px;
+  text-transform: uppercase;
+  -webkit-font-smoothing: subpixel-antialiased;
+  left: 0;
+  border-radius: 4px;
+  color: #fff;
+  cursor: pointer;
+  font-weight: bold;
+  box-shadow: 0 16px 26px -10px rgba(244, 67, 54, .56), 0 4px 25px 0 rgba(0, 0, 0, .12), 0 8px 10px -5px rgba(244, 67, 54, .2);
+  min-width: 140px;
+}
+
+.wizard-footer {
+  padding: 0 15px;
+}
+
+.btn-previous {
+  background-color: #999;
+  color: #fff;
+}
 </style>
