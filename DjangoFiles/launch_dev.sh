7f9ac8ec2ee5895954bca909b20790380ef2d15d
Jonas Legion <jturbeaux@pm.me>
2023-10-18 15:58:33 +0200

    volumes sql

--- DjangoFiles/launch_dev.sh
+++ DjangoFiles/launch_dev.sh
@@ -5,6 +5,17 @@ touch /DjangoFiles/logs/nginxError.log
 touch /DjangoFiles/logs/gunicorn.logs
 touch /DjangoFiles/logs/Djangologfile
 
+python /DjangoFiles/manage.py collectstatic --noinput
+
+export PGPASSWORD=$POSTGRES_PASSWORD
+export PGUSER=$POSTGRES_USER
+export PGHOST=$POSTGRES_HOST
+
+export LOAD_SQL=$LOAD_SQL
+psql --dbname $POSTGRES_DB -f $LOAD_SQL
+
+echo "SQL file loaded : $LOAD_SQL"
 python /DjangoFiles/manage.py migrate
+echo "Run rsp pour lancer le serveur web tout neuf"
 sleep infinity
 #python /DjangoFiles/manage.py runserver 0.0.0.0:8002
