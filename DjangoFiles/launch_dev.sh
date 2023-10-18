81452732f419eb4e86829584288d3c88c550f19d
Jonas Legion <jturbeaux@pm.me>
2023-10-18 15:44:55 +0200

    rezet bill

--- DjangoFiles/launch_dev.sh
+++ DjangoFiles/launch_dev.sh
@@ -1,15 +1,10 @@
-python /DjangoFiles/manage.py collectstatic --no-input
-python /DjangoFiles/manage.py migrate
-echo "Création des tenants public et meta"
-python /DjangoFiles/manage.py create_public
-echo "Création du super utilisateur"
-python /DjangoFiles/manage.py test_user
-
+set -e
 mkdir -p /DjangoFiles/logs
 touch /DjangoFiles/logs/nginxAccess.log
 touch /DjangoFiles/logs/nginxError.log
 touch /DjangoFiles/logs/gunicorn.logs
 touch /DjangoFiles/logs/Djangologfile
 
+python /DjangoFiles/manage.py migrate
 sleep infinity
 #python /DjangoFiles/manage.py runserver 0.0.0.0:8002
