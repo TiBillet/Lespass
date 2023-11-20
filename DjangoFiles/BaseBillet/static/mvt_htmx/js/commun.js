84a0227935c924b38143fb1b463654704af3a66f
filaos974 <filaos974@hotmail.com>
2023-11-20 17:29:27 +0400

    le script de l'adhésion est poussé dans le commun.js

--- DjangoFiles/BaseBillet/static/mvt_htmx/js/commun.js
+++ DjangoFiles/BaseBillet/static/mvt_htmx/js/commun.js
@@ -2,18 +2,15 @@ console.log('-> commun.js');
 const domain = `${window.location.protocol}//${window.location.host}`;
 console.log('domain =', domain);
 
-const initState = {
-    page: 'inconnue',
-
-}
-
-let state = new Proxy(initState, {
-  set(target, name, value) {
-    console.log("set " + name + " to " + value);
-    target[name] = value;
+ // une fois l'élément remplacé par le contenu de la requête
+ htmx.on("htmx:afterSettle", (evt) => {
+  console.log('-> htmx:afterSwap evt =', evt);
+  
+  if (evt.target.id === "tibillet-membership-modal") {
+    const elementModal = document.querySelector(
+      "#tibillet-membership-modal",
+    );
+    const modal = bootstrap.Modal.getOrCreateInstance(elementModal); // Returns a Bootstrap modal instance
+    modal.show();
   }
 });
-
-console.log('1 -> state =', state);
-state.page = 'accueil'
-console.log('2 -> state =', state);
