63d4bc8bf955dbe8bbf6068b32cf9cd1396a4c62
Jonas Legion <jturbeaux@pm.me>
2023-11-24 09:52:15 +0100

    Remove duplicate 'birth_date' field in serializers
    The 'birth_date' field was duplicated in the AuthBillet serializers. This commit removes the repetition to clean up the code and prevent any potential errors or inconsistencies.

--- DjangoFiles/BaseBillet/urls.py
+++ DjangoFiles/BaseBillet/urls.py
@@ -4,17 +4,17 @@ from BaseBillet import views as base_view
 
 urlpatterns = [
     path('ticket/<uuid:pk_uuid>/', base_view.Ticket_html_view.as_view()),
-    path('mvt/event/<slug:slug>/', base_view.event, name='event'),
+    path('event/<slug:slug>/', base_view.event, name='event'),
 
-    path('mvt/membership_form/<uuid:uuid>/', base_view.membership_form.as_view(), name='membership_form'),
+    path('membership_form/<uuid:uuid>/', base_view.membership_form.as_view(), name='membership_form'),
 
-    path('mvt/create_product/', base_view.create_product, name='create_product'),
-    path('mvt/home/', base_view.home, name='home'),
-    path("mvt/memberships/", base_view.memberships, name='memberships'),
-    path('mvt/test_jinja/', base_view.test_jinja, name='test_jinja'),
-    path('mvt/login/', base_view.login, name='login'),
-    path('mvt/emailconfirmation/<str:uuid>/<str:token>/',base_view.emailconfirmation, name='emailconfirmation'),
-    path('mvt/showModalMessageInEnterPage/', base_view.showModalMessageInEnterPage, name='showModalMessageInEnterPage'),
+    path('create_product/', base_view.create_product, name='create_product'),
+    path('home/', base_view.home, name='home'),
+    path("memberships/", base_view.memberships, name='memberships'),
+    path('test_jinja/', base_view.test_jinja, name='test_jinja'),
+    path('login/', base_view.login, name='login'),
+    path('emailconfirmation/<str:uuid>/<str:token>/',base_view.emailconfirmation, name='emailconfirmation'),
+    path('showModalMessageInEnterPage/', base_view.showModalMessageInEnterPage, name='showModalMessageInEnterPage'),
 
-    path('', base_view.index.as_view(), name="index"),
+    path('', base_view.home, name="index"),
 ]
