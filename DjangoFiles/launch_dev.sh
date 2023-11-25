4ef21fb927ec10893ac62a282c878e280e0ce1bc
Jonas Legion <jturbeaux@pm.me>
2023-11-25 16:29:28 +0100

    add backup script

--- DjangoFiles/launch_dev.sh
+++ DjangoFiles/launch_dev.sh
@@ -8,18 +8,18 @@ touch /DjangoFiles/logs/Djangologfile
 #sleep infinity
 
 DB_NAME=$POSTGRES_DB
-#if psql -lqtA | cut -d\| -f1 | grep -qxF "$DB_NAME"; then
-#  echo "La base de données ${DB_NAME} existe déjà"
-#else
-#  echo "La base de données ${DB_NAME} n'existe pas"
-##  pg_restore -f $LOAD_SQL
-#  export PGPASSWORD=$POSTGRES_PASSWORD
-#  export PGUSER=$POSTGRES_USER
-#  export PGHOST=$POSTGRES_HOST
-#  export LOAD_SQL=$LOAD_SQL
-#  psql --dbname $POSTGRES_DB -f $LOAD_SQL
-#  echo "SQL file loaded : $LOAD_SQL"
-#fi
+if psql -lqtA | cut -d\| -f1 | grep -qxF "$DB_NAME"; then
+  echo "La base de données ${DB_NAME} existe déjà"
+else
+  echo "La base de données ${DB_NAME} n'existe pas"
+#  pg_restore -f $LOAD_SQL
+  export PGPASSWORD=$POSTGRES_PASSWORD
+  export PGUSER=$POSTGRES_USER
+  export PGHOST=$POSTGRES_HOST
+  export LOAD_SQL=$LOAD_SQL
+  psql --dbname $POSTGRES_DB -f $LOAD_SQL
+  echo "SQL file loaded : $LOAD_SQL"
+fi
 
 
 # poetry run python /DjangoFiles/manage.py collectstatic --noinput
