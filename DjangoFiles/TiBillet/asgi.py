1274bbc8b33f0bab30ec5bc10cf20c37b4fc28ae
Jonas Legion <jturbeaux@pm.me>
2023-11-27 10:44:16 +0100

    add poetry for ws

--- DjangoFiles/TiBillet/asgi.py
+++ DjangoFiles/TiBillet/asgi.py
@@ -5,20 +5,32 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-import wsocket.routing
-from wsocket.middlewares import WebSocketJWTAuthMiddleware
+from wsocket.routing import websocket_urlpatterns
+# from wsocket.middlewares import WebSocketJWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TiBillet.settings')
 
-application = ProtocolTypeRouter({
-
-    "http": get_asgi_application(),
-
-    "websocket": AllowedHostsOriginValidator(
-        WebSocketJWTAuthMiddleware(
-            URLRouter(
-                wsocket.routing.websocket_urlpatterns
-            )
+django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
-    ),
-})
+    }
+)
+
+
+# EX TiBillet Test
+# application = ProtocolTypeRouter({
+#
+#     "http": get_asgi_application(),
+#
+#     "websocket": AllowedHostsOriginValidator(
+#         WebSocketJWTAuthMiddleware(
+#             URLRouter(
+#                 wsocket.routing.websocket_urlpatterns
+#             )
+#         ),
+#     ),
+# })
--- DjangoFiles/install.sh
+++ DjangoFiles/install.sh
@@ -1,5 +1,5 @@
-python /DjangoFiles/manage.py collectstatic --no-input
-python /DjangoFiles/manage.py migrate
-python /DjangoFiles/manage.py create_public
+poetry run python /DjangoFiles/manage.py collectstatic --no-input
+poetry run python /DjangoFiles/manage.py migrate
+poetry run python /DjangoFiles/manage.py create_public
 echo "Création du super utilisateur :"
-python /DjangoFiles/manage.py create_tenant_superuser -s public
+poetry run python /DjangoFiles/manage.py create_tenant_superuser -s public
