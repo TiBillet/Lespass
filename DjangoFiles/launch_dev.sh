7ed7a896595438f80205419e43f14ea75a7ec9d1
Jonas Legion <jturbeaux@pm.me>
2023-11-29 10:37:44 +0100

    dev script

--- DjangoFiles/launch_dev.sh
+++ DjangoFiles/launch_dev.sh
@@ -5,24 +5,20 @@ touch /DjangoFiles/logs/nginxError.log
 touch /DjangoFiles/logs/gunicorn.logs
 touch /DjangoFiles/logs/Djangologfile
 
-#sleep infinity
-
 DB_NAME=$POSTGRES_DB
+export PGPASSWORD=$POSTGRES_PASSWORD
+export PGUSER=$POSTGRES_USER
+export PGHOST=$POSTGRES_HOST
+export LOAD_SQL=$LOAD_SQL
 if psql -lqtA | cut -d\| -f1 | grep -qxF "$DB_NAME"; then
   echo "La base de données ${DB_NAME} existe déjà"
 else
   echo "La base de données ${DB_NAME} n'existe pas"
-#  pg_restore -f $LOAD_SQL
-  export PGPASSWORD=$POSTGRES_PASSWORD
-  export PGUSER=$POSTGRES_USER
-  export PGHOST=$POSTGRES_HOST
-  export LOAD_SQL=$LOAD_SQL
   psql --dbname $POSTGRES_DB -f $LOAD_SQL
   echo "SQL file loaded : $LOAD_SQL"
 fi
 
 
-# poetry run python /DjangoFiles/manage.py collectstatic --noinput
 poetry run python /DjangoFiles/manage.py migrate
 echo "Run rsp pour lancer le serveur web tout neuf"
 sleep infinity
