3f78b8786e40597ae691486700aea2e195742d1d
Jonas Legion <jturbeaux@pm.me>
2024-01-19 10:28:03 +0100

    Faute

--- DjangoFiles/launch_dev.sh
+++ DjangoFiles/launch_dev.sh
@@ -10,18 +10,28 @@ export PGPASSWORD=$POSTGRES_PASSWORD
 export PGUSER=$POSTGRES_USER
 export PGHOST=$POSTGRES_HOST
 export LOAD_SQL=$LOAD_SQL
-if psql -lqtA | cut -d\| -f1 | grep -qxF "$DB_NAME"; then
-  echo "La base de données ${DB_NAME} existe déjà"
-else
-  echo "La base de données ${DB_NAME} n'existe pas"
-  sleep 3
-  psql --dbname $POSTGRES_DB -f $LOAD_SQL
-  echo "SQL file loaded : $LOAD_SQL"
-fi
 
+load_sql() {
+  if psql -lqtA | cut -d\| -f1 | grep -qxF "$DB_NAME"; then
+    echo "La base de données ${DB_NAME} existe déjà"
+  else
+    echo "La base de données ${DB_NAME} n'existe pas"
+    sleep 3
+    psql --dbname $POSTGRES_DB -f $LOAD_SQL
+    echo "SQL file loaded : $LOAD_SQL"
+  fi
 
-poetry run python /DjangoFiles/manage.py migrate
-#echo "Run rsp pour lancer le serveur web tout neuf"
+}
+
+# Fonction qui sera exécutée en cas d'erreur
+handle_error() {
+  echo "Une erreur s'est produite dans la ligne $1"
+  sleep infinity
+}
+
+trap 'handle_error $LINENO' ERR
+# poetry run python /DjangoFiles/manage.py migrate
+# echo "Run rsp pour lancer le serveur web tout neuf"
 sleep infinity
 
 #python /DjangoFiles/manage.py runserver 0.0.0.0:8002
