360e3dfaf99023b4e18b0ad45dbcb2652a25e3cb
Jonas Legion <jturbeaux@pm.me>
2024-02-15 17:58:27 +0100

    BIG REFACTO - Federated system erased

--- DjangoFiles/fedow_connect/fedow_api.py
+++ DjangoFiles/fedow_connect/fedow_api.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 from AuthBillet.models import RsaKey, TibilletUser
 from BaseBillet.models import Configuration
+from fedow_connect.models import FedowConfig
 from fedow_connect.utils import sign_message, data_to_b64, verify_signature
 
 logger = logging.getLogger(__name__)
@@ -47,13 +48,12 @@ def _post(config, path, data):
     return request_fedow
 
 
-def _get(config, path: list, arg=None, param=None):
+def _get(config: FedowConfig, user: TibilletUser, path: str):
     fedow_domain = config.fedow_domain
     fedow_place_admin_apikey = config.fedow_place_admin_apikey
-    PATH = f'/{"/".join(path)}/'
 
     # Signature de la requete : on signe le path
-    private_key = config.get_private_key()
+    private_key = user.get_private_key()
     # Signature de la requete : on signe la clé
 
     signature = sign_message(
@@ -63,13 +63,13 @@ def _get(config, path: list, arg=None, param=None):
 
     # Ici, on s'autovérifie :
     # Assert volontaire. Si non effectué en prod, ce n'est pas grave.
-    assert verify_signature(config.get_public_key(),
+    assert verify_signature(user.get_public_key(),
                             fedow_place_admin_apikey.encode('utf8'),
                             signature)
 
     session = requests.Session()
     request_fedow = session.get(
-        f"https://{fedow_domain}{PATH}",
+        f"https://{fedow_domain}/{path}/",
         headers={
             'Authorization': f'Api-Key {fedow_place_admin_apikey}',
             "Signature": f"{signature}",
@@ -82,27 +82,20 @@ def _get(config, path: list, arg=None, param=None):
 
 
 class WalletFedow():
-    def __init__(self, context):
-        config:Configuration= context.get('config')
-        rsa_key:RsaKey= context.get('rsa_key')
-        if not all([config, rsa_key]):
-            raise Exception("config and key are required")
+    def __init__(self, config):
+        config: FedowConfig = config
+        if not config:
+            config = FedowConfig.get_solo()
 
-    def get_or_create(self, user:TibilletUser):
-        pass
+    def get_or_create(self, user: TibilletUser):
+        email = user.email
+        pub_key = user.get_public_key()
 
 
 class FedowAPI():
-    def __init__(self, rsa_key: RsaKey = None, config=None):
+    def __init__(self, config: FedowConfig = None):
         self.config = config
         if config is None:
-            self.config = Configuration.get_solo()
-        self.rsa_key = rsa_key
-        if rsa_key is None:
-            raise Exception("key is required")
-        context = {
-            'config': self.config,
-            'rsa_key': self.rsa_key,
-        }
+            self.config = FedowConfig.get_solo()
 
-        self.wallet = WalletFedow(context)
+        self.wallet = WalletFedow(self.config)
