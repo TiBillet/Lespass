6440434d72bed7a1a96cf023f37d4523a7562f10
Jonas Legion <jturbeaux@pm.me>
2024-09-04 18:59:53 +0200

    new tenant

--- Administration/management/commands/new_tenant.py
+++ Administration/management/commands/new_tenant.py
@@ -1,4 +1,4 @@
-import logging
+import logging, os
 
 from django.core.management.base import BaseCommand
 from django_tenants.utils import schema_context
@@ -12,19 +12,21 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-
         parser.add_argument('--name',
                             help='Tenant name')
         parser.add_argument('--email',
                             help='Admin email')
+        parser.add_argument('--dns',
+                        help='DNS')
 
     def handle(self, *args, **options):
         with schema_context('meta'):
+            dns_choice = options['dns'] if options.get('dns') else os.environ.get('DOMAIN')
             waiting_config = WaitingConfiguration.objects.create(
                 organisation=options['name'],
                 slug=slugify(options['name']),
                 email=options['email'],
-                dns_choice='tibillet.coop',
+                dns_choice=dns_choice,
             )
             TenantCreateValidator.create_tenant(waiting_config)
         logger.info('Tenant created: %s', waiting_config)
