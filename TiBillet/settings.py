96ff034f5453b548a8d43c109882cb6623ce6312
JonasFW13 <jturbeaux@pm.me>
2025-02-08 16:10:43 +0100

    rate

--- TiBillet/settings.py
+++ TiBillet/settings.py
@@ -229,8 +229,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
+        'anon': '10000/day',
+        'user': '1000000/day'
     },
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
