3bafef41a071d1b3d91e5f386c120efdf6295dcd
JonasFW13 <jturbeaux@pm.me>
2025-02-23 17:01:52 +0100

    ticket V1

--- BaseBillet/tasks.py
+++ BaseBillet/tasks.py
@@ -301,8 +301,8 @@ def create_ticket_pdf(ticket: Ticket):
     }
 
     # template_name = 'report/report.html'
-    # template_name = 'ticket/ticket.html'
-    template_name = 'ticket/ticket_V2.html'
+    template_name = 'ticket/ticket.html'
+    # template_name = 'ticket/ticket_V2.html'
 
     # template_name = 'ticket/example_flight_ticket.html'
     font_config = FontConfiguration()
