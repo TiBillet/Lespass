39605954af9d4417e8e2d352981d012b56174bd6
JonasFW13 <jturbeaux@pm.me>
2025-06-04 15:08:36 +0200

    Add Formbricks API integration and support for booking panel auto-opening
    - Implement `FormbricksAPI` library for interacting with Formbricks API (V1 and V2) with features like form listing, response retrieval, and user creation.
    - Update event template to detect `openbookingPanel` URL parameter and automatically show the booking panel when present.
    - Adjust external tools module to include `FormbricksAPI`.

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ poetry install
 echo "Poetry install ok"
 
 poetry run python /DjangoFiles/manage.py collectstatic --no-input
-#poetry run python /DjangoFiles/manage.py migrate
+poetry run python /DjangoFiles/manage.py migrate
 
 # Création des tenant publics et agenda
-#poetry run python /DjangoFiles/manage.py install
+poetry run python /DjangoFiles/manage.py install
 
 echo "Gunicorn start"
 poetry run gunicorn TiBillet.wsgi --log-level=info --access-logfile /DjangoFiles/logs/gunicorn.logs --log-file /DjangoFiles/logs/gunicorn.logs --error-logfile /DjangoFiles/logs/gunicorn.logs --log-level info --capture-output --reload -w 5 -b 0.0.0.0:8002
