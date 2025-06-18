281c48af82c640abcede8af0d88714b357d7540a
JonasFW13 <jturbeaux@pm.me>
2025-06-18 19:41:16 +0200

    Add custom error handlers and improve template translations
    - Registered custom handlers for HTTP 404 and 500 errors in `urls_tenants.py` and `BaseBillet/urls.py`.
    - Added test routes to simulate these errors for debugging purposes.
    - Updated and refined translations in `django.po`, including error-related messages.
    - Enhanced templates for better error messaging in `404.html` and `500.html`.

--- TiBillet/settings.py
+++ TiBillet/settings.py
@@ -32,7 +32,6 @@ FERNET_KEY = os.environ.get('FERNET_KEY')
 FEDOW = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == '1'
-DEBUG = False
 
 TEST = os.environ.get('TEST') == '1'
 
