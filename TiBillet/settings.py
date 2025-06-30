0df93e288336cdfb9937f2cdf59de62c806558a5
JonasFW13 <jturbeaux@pm.me>
2025-06-30 17:35:48 +0200

    test CORS

--- TiBillet/settings.py
+++ TiBillet/settings.py
@@ -88,8 +88,8 @@ if os.environ.get('ADDITIONAL_DOMAINS'):
 
 
 CORS_ORIGIN_WHITELIST = CSRF_TRUSTED_ORIGINS
-"""
 CORS_ORIGIN_WHITELIST = CSRF_TRUSTED_ORIGINS + ['http://localhost', 'http://localhost:8000', 'http://localhost:8080']
+"""
 CORS_ALLOW_ALL_ORIGINS = True
 """
 CORS_ALLOW_CREDENTIALS = True
