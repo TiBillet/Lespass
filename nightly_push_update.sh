1d285b5c3ddec11c392980bd80a7296a18f28e20
JonasFW13 <jturbeaux@pm.me>
2025-10-21 10:38:19 +0200

    add versioning and nightly build script for Docker deployment

--- nightly_push_update.sh
+++ nightly_push_update.sh
@@ -8,17 +8,8 @@ cd "$SCRIPT_DIR"
 readonly IMAGE_NAME="lespass"
 readonly DOCKER_USER="tibillet"
 
-if [[ -f VERSION ]]; then
-  VERSION="$(< VERSION)"
-else
-  echo "VERSION file not found next to this script" >&2
-  exit 1
-fi
-
 git checkout PreProd
 git pull
 
-docker build -t "$DOCKER_USER/$IMAGE_NAME:nightly" -t "$DOCKER_USER/$IMAGE_NAME:nightly-$VERSION" .
-
+docker build -t "$DOCKER_USER/$IMAGE_NAME:nightly" .
 docker push "$DOCKER_USER/$IMAGE_NAME:nightly"
-docker push "$DOCKER_USER/$IMAGE_NAME:nightly-$VERSION"
